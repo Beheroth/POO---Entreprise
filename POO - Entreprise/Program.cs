7047f38dd6aa819a55bc1ec84707f1f8267a01ff
Beheroth <arnaud.peeters95@gmail.com>
2017-10-27 16:44:18 +0200

    Added Main methods

--- POO - Entreprise/Program.cs
+++ POO - Entreprise/Program.cs
@@ -10,6 +10,14 @@ namespace POO___Entreprise
     {
         static void Main(string[] args)
         {
+            DateTime date = new DateTime(2017, 10, 23);
+            String name = "GE";
+
+            Generator gen = new Generator();
+            Entreprise entreprise = gen.GenerateAll(name, date);
+
+            InterfaceClient show = new InterfaceClient(entreprise);
+            show.Start();
         }
     }
 }
